fec3e00df4c64a99e0dd6eb6b0c618516949836f
Karan Kumar <kumarkay.ca@gmail.com>
2019-09-30 20:14:46 -0400

    Calculate Age Method Added

--- src/main/java/com/group1/Employee.java
+++ src/main/java/com/group1/Employee.java
@@ -3,7 +3,11 @@ package com.group1;
 public abstract class Employee
 {
     int id;
-    String Name;
-    int Age;
+    String name;
+    int age;
+
+    public int calBirthYear(){
+        return age;
+    }
 
 }
