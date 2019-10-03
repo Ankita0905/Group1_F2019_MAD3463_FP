50e041f2c7642b4e3da1a074049f774146ef8dca
Ankita Jain <ankita.jain12342@gmail.com>
2019-10-03 18:02:28 -0400

    creating objects in main class

--- src/main/java/com/group1/MainClass.java
+++ src/main/java/com/group1/MainClass.java
@@ -4,7 +4,8 @@ public class MainClass
 {
     public static void main(String[] args)
     {
-
+        Intern i1 = new Intern("Ankita",  24, "Lambton College");
+        i1.printMyData();
 
     }
 }
