9794ff8b242e1952dad27f744b4f0883cb46301c
macstudent <kaurkamaljit963@gmail.com>
2019-10-03 20:16:14 -0400

    getter

--- src/main/java/com/group1/Vehicle/Car.java
+++ src/main/java/com/group1/Vehicle/Car.java
@@ -2,14 +2,17 @@ package com.group1.Vehicle;
 
 public class Car extends Vehicle
 {
-    private String color;
-    private int noOfSeats;
-    private int maxSpeed;
+    private String color="Black";
+    private int noOfSeats=4;
+    private int maxSpeed=350;
     public Car(String make, String model, int year, String plateNum, String typeOfVehicle) {
         super(make, model, year, plateNum, typeOfVehicle);
     }
     public String getColor() {
         return color;
+    }
+    public int getNoOfSeats() {
+        return noOfSeats;
     }
 
     @Override
