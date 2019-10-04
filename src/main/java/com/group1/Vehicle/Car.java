88806d4716de284cb7b215d5323e120f91a96463
macstudent <kaurkamaljit963@gmail.com>
2019-10-03 20:13:47 -0400

    variables declared fot car

--- src/main/java/com/group1/Vehicle/Car.java
+++ src/main/java/com/group1/Vehicle/Car.java
@@ -2,4 +2,12 @@ package com.group1.Vehicle;
 
 public class Car extends Vehicle
 {
+    private String color;
+    private int noOfSeats;
+    private int maxSpeed;
+
+    @Override
+    public void printMyData() {
+
+    }
 }
