1827011f1d88e9bea2518dc19c814e737aa6e59a
macstudent <kaurkamaljit963@gmail.com>
2019-10-03 20:25:48 -0400

    printing data

--- src/main/java/com/group1/Vehicle/Car.java
+++ src/main/java/com/group1/Vehicle/Car.java
@@ -2,25 +2,17 @@ package com.group1.Vehicle;
 
 public class Car extends Vehicle
 {
-    private String color="Black";
-    private int noOfSeats=4;
-    private int maxSpeed=350;
+
+
     public Car(String make, String model, int year, String plateNum, String typeOfVehicle) {
         super(make, model, year, plateNum, typeOfVehicle);
-    }
-    public String getColor() {
-        return color;
-    }
-    public int getNoOfSeats() {
-        return noOfSeats;
-    }
-    public int getMaxSpeed() {
-        return maxSpeed;
     }
 
 
     @Override
     public void printMyData() {
+        System.out.println("make: " +getMake());
+        System.out.println("model: " +getModel());
 
     }
 }
