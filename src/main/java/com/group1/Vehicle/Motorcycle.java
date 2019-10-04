9e881a217ec192b3fac53667cf0a4c14a1b9f3e4
SandeepSingh2810 <sandeepjangra0100@gmail.com>
2019-10-03 20:36:41 -0400

    constructor in motorcycle

--- src/main/java/com/group1/Vehicle/Motorcycle.java
+++ src/main/java/com/group1/Vehicle/Motorcycle.java
@@ -3,7 +3,7 @@ package com.group1.Vehicle;
 public class Motorcycle extends Vehicle
 {
     public Motorcycle(String make, String model, int year, String plateNum, String typeOfVehicle) {
-
+        super(make, model, year, plateNum, typeOfVehicle);
     }
 
 }
